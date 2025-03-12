fe2ad2cb8116bb31379ddd1c7d91dc901a740169
yamihaha <2579010536@qq.com>
2025-03-12 08:37:39 +0000

    original code

---
+++ my_make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "check the kernel name?(y/n): " answer
+
+# 检查用户输入
+if [ "$answer" = "y" ]; then
+    echo "start compilling..."
+    sudo make clean
+    bear make -j16
+    sudo make INSTALL_MOD_STRIP=1 modules_install
+    sudo make install
+else
+    exit 1
+fi
